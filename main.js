28b5ef6c40f932664483f1026cfef18c87e8ecf1
Emily Venturino <ebventurino@gmail.com>
2018-06-28 10:51:02 -0500

    finished cards

--- main.js
+++ main.js
@@ -77,46 +77,55 @@ for (let i = 0; i < employees.length; i++) {
     const newSection = document.createElement("h2")
     const newStatement = document.createTextNode(employees[i].name);
     newSection.appendChild(newStatement);
-    document.body.appendChild(newSection);
     console.log(employees[i].department)
+    const newDiv = document.createElement("div")
+    newDiv.appendChild(newSection);
+
+    // WORKS IN TECH 
     if (employees[i].department === 1) {
         const newSection1 = document.createElement("section")
-        const newStatement1 = document.createTextNode("Tech");
+        const newStatement1 = document.createTextNode("Works in Tech");
         newSection1.appendChild(newStatement1);
-        document.body.appendChild(newSection1);
+        newDiv.appendChild(newSection1);
+    
+    // WORKS IN MARKETING
     } else if (employees[i].department === 2) {
         const newSection2 = document.createElement("section")
-        const newStatement2 = document.createTextNode("Marketing");
+        const newStatement2 = document.createTextNode("Works In Marketing");
         newSection2.appendChild(newStatement2);
-        document.body.appendChild(newSection2);
+        newDiv.appendChild(newSection2);
+
+    // WORKS IN DESIGN
     } else {
         const newSection3 = document.createElement("section")
-        const newStatement3 = document.createTextNode("Design");
+        const newStatement3 = document.createTextNode("Works In Design");
         newSection3.appendChild(newStatement3);
-        document.body.appendChild(newSection3);
+        newDiv.appendChild(newSection3);
 
     }
+    // USES A PC
     if (employees[i].computer === 1) {
         const newSection4 = document.createElement("section")
-        const newStatement4 = document.createTextNode("PC");
+        const newStatement4 = document.createTextNode("Uses a PC");
         newSection4.appendChild(newStatement4);
-        document.body.appendChild(newSection4);
+        newDiv.appendChild(newSection4);
+
+    // USES A LAPTOP
     } else {
         const newSection5 = document.createElement("section")
-        const newStatement5 = document.createTextNode("Laptop");
+        const newStatement5 = document.createTextNode("Uses a Laptop");
         newSection5.appendChild(newStatement5);
-        document.body.appendChild(newSection5);
+        newDiv.appendChild(newSection5);
 
     }
-  
+    document.body.appendChild(newDiv);
+   
+
 
 }
-
-
-
-
-
-
-
-
-    //nested for loop with an if else statement
+const employeeId = document.querySelectorAll("h2");
+for (let i = 0; i < employeeId.length; i++) {
+    employeeId[i].classList.add("employee_name")
+    
+    
+}
